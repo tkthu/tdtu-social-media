b1f3321d3ab09501d23d6f9ebf17778b6d38069f
Trần Ngọc Phương Nhi <tranngocphuongnhi0412@gmail.com>
2021-03-15 21:25:21 +0700

    chỉnh sửa và bổ sung view

---
+++ .history/index_20210315163623.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const credentials = require('./credentials');
+const route = require('./routes/index');
+
+const db = require('./config/db/index');
+db.connect();
+
+const app = express();
+app.engine('.hbs', exphbs({
+    extname: ".hbs",
+    helpers: {
+        inc: function(value, options){return parseInt(value) + 1;},
+        ifEquals: function(arg1, arg2, options) {
+            return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+        }
+    }
+}));
+app.set('view engine', '.hbs');
+
+app.use(express.static(path.join(__dirname , 'public')));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({ saveUninitialized: false, resave:true, secret: credentials.sessionSecret }));
+app.use(require('express-flash')());
+
+
+route(app);
+
+
+app.listen(credentials.port,()=>{console.log(`http://localhost:${credentials.port}`)})
