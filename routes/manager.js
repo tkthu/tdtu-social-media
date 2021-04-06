1d39a4678b51d7620d4ca5f85f38179c0e2d7123
Trần Ngọc Phương Nhi <tranngocphuongnhi0412@gmail.com>
2021-04-06 09:00:22 +0700

    thêm tài khoản phòng khoa

--- routes/manager.js
+++ routes/manager.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const managerController = require('../src/controllers/ManagerController');
+const {upload} = require('../util/middlewares/fileUpload')
 
 router.get('/staffs',managerController.staffsPage);// trang quản lý tài khoản nhân viên (tài khoản phòng/khoa)
+router.post('/staffs',upload.fields([{name:"image"}]),managerController.staffsAdd);// Thêm tài khoản staffs
 router.get('/students',managerController.studentsPage);// trang quản lý tài khoản sinh viên
 
 module.exports = router
--- src/controllers/ManagerController.js
+++ src/controllers/ManagerController.js
@@ -1,19 +1,75 @@
 const Users = require('../models/user.model')
+const Departments = require('../models/department.model')
 const { multipleMongooseToObject } = require('../../util/mongoose')
+const bcrypt = require('bcrypt')
+const mogoose = require('mongoose')
+
 class ManagerController{
 
     // [GET] /staffs
-    staffsPage(req, res, next){        
-        Users.find({userType: "staff"}) // Tìm user với userType là "staff"
+    staffsPage(req, res, next){    
+        var {search} = req.query
+        // var data = Users.Filter((item) => {
+        //     return item.search
+        // })    
+        Users.find({userType: "staff"})
         .then((users) => {
-            res.render('admin-acc-phong-khoa', {
+            req.users = users;
+            return Departments.find();
+        })
+        .then((departments) => {
+            res.render('admin-acc-phong-khoa', { 
+                department: multipleMongooseToObject(departments),
                 user: req.user,
-                users: multipleMongooseToObject(users)
-            });
+                users: multipleMongooseToObject(req.users),
+                // data: data
+            })
         })
         .catch(next);
+
+        
+
     }
 
+    // [POST] /staffs
+    staffsAdd(req, res, next){
+        var imagesArray = undefined;
+        if(req.files.image !== undefined) {
+            imagesArray = req.files.image.map( fi => {
+                return fi.path.replace("public","");
+            })
+        }
+
+        const addStaff = {
+            _id: mogoose.Types.ObjectId(),
+            username: req.body.username,
+            password: bcrypt.hashSync(req.body.password,10),
+            displayName: req.body.displayName,
+            imagesArray,
+            createdAt: new Date().toISOString(),
+            staffInfo: {
+                authorized: req.body.chuyenmuc,
+            },
+            userType: "staff",
+        }
+
+        new Users(addStaff).save()
+        .then((resultAdd) => {
+            return res.status(200).json({
+                code:0,
+                msg:'Thêm staff thành công',
+                data: {
+                    addStaff
+                }
+            });
+        })
+        .catch(err => {
+            return res.status(500).json({
+                msg:'Thêm mới staff thất bại với lỗi ' + err,
+            });
+        })
+    }
+ 
     // [GET] /students
     studentsPage(req, res, next){       
         Users.find({userType: "student"})  // Tìm user với userType là "student"
