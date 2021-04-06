eeb0835b3b404d25a3d69f7be92e3568f5dfac08
toan xe <toanriven1507@gmail.com>
2021-04-06 20:30:28 +0700

    post-notifi
    Post de xem lai

--- src/controllers/NotificationsController.js
+++ src/controllers/NotificationsController.js
@@ -1,22 +1,44 @@
-const post =  require('../models/post.model')
+const postNofi =  require('../models/post.model')
+const commentModel = require('../models/comment.model');
 class NotificationsController{
 
     // [GET] /
     index(req, res){      
-        post.find({})
-            .then(post => {
-                if (post === null){
-                    throw new Error('not found posts')
+        
+        postNofi.find({})
+        .then( postsFound => {
+            if (postsFound === null){
+                throw new Error('not found posts')
+            }
+            var posts = postsFound.map( post => {  // ông psu hlen6 thử. để tui coi máy tui chạy được ko okieelaaa push rồi á
+                
+
+                return {// ông trueyn26 cái này vô. thì giờ lấy ra sài thôi
+                    _id: post._id,
+                    name: post.name,
+                    content: post.content,                
+                    createdAt: post.createdAt,
+                    lastEdited: post.lastEdited,
+                    imagesArray: post.imagesArray,
+                    attachmentsArray: post.attachmentsArray,
+                    commentsCount: post.commentsCount,
+                    department: post.department,
+                    sender: post.sender,
                 }
 
-                res.render('all-notification',{ post })
             })
-            .catch(err => {
-                return res.end("somthing went wrong ... | "+err);
-            })
+            console.log("postsFound ", postsFound)// dòng này chạy được mà
+            res.render("all-notification",{postsFound});// ông truyền postsFound vào all-notification.hbs
+        })
+             
+        .catch(err => {
+            return res.end("somthing went wrong ... | "+err);
+        })
         
     }
 
+
+    
     // [GET] /departments
     departmentPage(req, res){        
         res.render("phongban",{user: req.user});
