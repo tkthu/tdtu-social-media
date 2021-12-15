c0f734c632f2c5e79485ca310ef22cd93785e465
tkthu <50312642+tkthu@users.noreply.github.com>
2021-12-15 22:00:58 +0700

    test

--- util/config/db/firebaseAdmin.js
+++ util/config/db/firebaseAdmin.js
@@ -5,6 +5,10 @@ var serviceAccount = require("../../../credentials/tdtu-social-media-firebase-ad
 serviceAccount.private_key_id = `${process.env.FIREBASE_PRIVATE_KEY_ID}`
 serviceAccount.private_key = `${process.env.FIREBASE_PRIVATE_KEY}`
 
+console.log('private_key_id ', serviceAccount.private_key_id)
+console.log('private_key', serviceAccount.private_key)
+console.log('serviceAccount', serviceAccount)
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "tdtu-social-media.appspot.com",
