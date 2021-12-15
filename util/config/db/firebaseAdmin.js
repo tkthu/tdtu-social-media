1f6a168c4b8d420496ebd08b4caf595a02b2afe0
tkthu <50312642+tkthu@users.noreply.github.com>
2021-12-16 00:21:37 +0700

    test

--- util/config/db/firebaseAdmin.js
+++ util/config/db/firebaseAdmin.js
@@ -1,24 +1,17 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../../../credentials/tdtu-social-media-firebase-adminsdk-5py4q-e584e5a09f.json");
-
-
-serviceAccount = {
+var serviceAccount = {
   "type": "service_account",
-  "project_id": "tdtu-social-media",
+  "project_id": "tdtu-social-media-web",
   "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
   "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g,'\n'),
-  "client_email": "firebase-adminsdk-5py4q@tdtu-social-media.iam.gserviceaccount.com",
-  "client_id": "107926622862175199695",
+  "client_email": "firebase-adminsdk-29j3d@tdtu-social-media-web.iam.gserviceaccount.com",
+  "client_id": "114073423452690687490",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-5py4q%40tdtu-social-media.iam.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-29j3d%40tdtu-social-media-web.iam.gserviceaccount.com"
 }
-// serviceAccount.private_key_id = 
-// // serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY.replace(/['"]+/g, '')
-
-console.log('serviceAccount', serviceAccount)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
