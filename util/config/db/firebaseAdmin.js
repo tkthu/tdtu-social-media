3b74db7919ae9562687c616fb5003458c2757345
tkthu <50312642+tkthu@users.noreply.github.com>
2021-12-15 22:05:53 +0700

    test

--- util/config/db/firebaseAdmin.js
+++ util/config/db/firebaseAdmin.js
@@ -2,8 +2,8 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("../../../credentials/tdtu-social-media-firebase-adminsdk-5py4q-e584e5a09f.json");
 
-serviceAccount.private_key_id = `${process.env.FIREBASE_PRIVATE_KEY_ID}`
-serviceAccount.private_key = `${process.env.FIREBASE_PRIVATE_KEY}`
+serviceAccount.private_key_id = process.env.FIREBASE_PRIVATE_KEY_ID
+serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY
 
 console.log('private_key_id ', serviceAccount.private_key_id)
 console.log('private_key', serviceAccount.private_key)
